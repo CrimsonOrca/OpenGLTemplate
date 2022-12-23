6722ee99708ab389a21edb0c17464b0cb7f0d55a
CrimsonOrca <b.miah0987@gmail.com>
2022-12-23 08:05:29 -0500

    Added point light...

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,13 +1,15 @@
 #include "OpenGLTemplate.h"
 
+void DrawCubes(Shader&, Renderer&);
+
 int main()
 {
 	Window window;
 
 	Time time;
 
-	glm::vec3 cameraPosition {0.0f, 0.0f, 10.0f};
-	Camera camera {cameraPosition};
+	glm::vec3 cameraPosition{ 0.0f, 0.0f, 10.0f };
+	Camera camera{ cameraPosition };
 
 	Shader materialsShader("Assets/Shaders/light-casters-vert.glsl", "Assets/Shaders/light-casters-frag.glsl");
 	materialsShader.Use();
@@ -18,19 +20,6 @@ int main()
 	Renderer renderer;
 	renderer.EnableDepthTesting();
 	renderer.SetMesh<Cube>();
-
-	Position cubePositions[] = {
-		Position( 0.0f,  0.0f,  0.0f),
-		Position( 2.0f,  5.0f, -15.0f),
-		Position(-1.5f, -2.2f, -2.5f),
-		Position(-3.8f, -2.0f, -12.3f),
-		Position( 2.4f, -0.4f, -3.5f),
-		Position(-1.7f,  3.0f, -7.5f),
-		Position( 1.3f, -2.0f, -2.5f),
-		Position( 1.5f,  2.0f, -2.5f),
-		Position( 1.5f,  0.2f, -1.5f),
-		Position(-1.3f,  1.0f, -1.5f)
-	};
 
 	while (!window.ShouldClose())
 	{
@@ -47,32 +36,64 @@ int main()
 		materialsShader.SetInt("uMaterial.specular", 1);
 
 		// set shader uniforms...
+		Transformation transformation;
 		materialsShader.SetMatrix("uView", camera.GetViewMatrix());
 		materialsShader.SetMatrix("uProjection", camera.GetProjectionMatrix());
 		materialsShader.SetVector("uViewPosition", camera.GetPosition());
+
 		materialsShader.SetFloat("uMaterial.shininess", 32.0f);
-		materialsShader.SetVector("uLight.direction", glm::normalize(glm::vec3(-0.2f, -1.0f, -0.3f)));
-		materialsShader.SetVector("uLight.ambient", Color(0.2f, 0.2f, 0.2f));
-		materialsShader.SetVector("uLight.diffuse", Color(0.5f, 0.5f, 0.5f));
-		materialsShader.SetVector("uLight.specular", Color(1.0f, 1.0f, 1.0f));
+
+		materialsShader.SetVector("uDirectionalLight.direction", glm::normalize(glm::vec3(-0.2f, -1.0f, -0.3f)));
+		materialsShader.SetVector("uDirectionalLight.light.ambient", Color(0.2f, 0.2f, 0.2f));
+		materialsShader.SetVector("uDirectionalLight.light.diffuse", Color(0.5f, 0.5f, 0.5f));
+		materialsShader.SetVector("uDirectionalLight.light.specular", Color(1.0f, 1.0f, 1.0f));
+
+		materialsShader.SetVector("uPointLight.light.ambient", Color(0.2f, 0.2f, 0.2f));
+		materialsShader.SetVector("uPointLight.light.diffuse", Color(0.5f, 0.5f, 0.5f));
+		materialsShader.SetVector("uPointLight.light.specular", Color(1.0f, 1.0f, 1.0f));
+		materialsShader.SetVector("uPointLight.position", Position(0.5f, 0.5f, 0.5f));
+		materialsShader.SetFloat("uPointLight.constant", 1.0f);
+		materialsShader.SetFloat("uPointLight.linear", 0.09f);
+		materialsShader.SetFloat("uPointLight.quadratic", 0.032f);
 
 		// rendering commands...
 		renderer.ClearScreen();
 
-		for (int i = 0; i < 10; i++)
-		{
-			Transformation model;
-			model.Translate(cubePositions[i].x, cubePositions[i].y, cubePositions[i].z);
-			float angle = 20.0f * i;
-			model.Rotate(glm::normalize(glm::vec3(1.0f, 0.3f, 0.5f)), angle);
-			materialsShader.SetMatrix("uModel", model.GetModelMatrix());
-			renderer.Draw();
-		}
-	
+		DrawCubes(materialsShader, renderer);
+
 		// check and call events, swap the front and back buffers...
 		window.SwapBuffers();
 		window.ProcessEvents();
 	}
 
 	return 0;
+}
+
+
+void DrawCubes(Shader& shader, Renderer& renderer)
+{
+	static const int numCubePositions = 10;
+
+	static const Position cubePositions[numCubePositions] = {
+		Position( 0.0f,  0.0f,  0.0f),
+		Position( 2.0f,  5.0f, -15.0f),
+		Position(-1.5f, -2.2f, -2.5f),
+		Position(-3.8f, -2.0f, -12.3f),
+		Position( 2.4f, -0.4f, -3.5f),
+		Position(-1.7f,  3.0f, -7.5f),
+		Position( 1.3f, -2.0f, -2.5f),
+		Position( 1.5f,  2.0f, -2.5f),
+		Position( 1.5f,  0.2f, -1.5f),
+		Position(-1.3f,  1.0f, -1.5f)
+	};
+
+	for (int i = 0; i < numCubePositions; i++)
+	{
+		Transformation transformation;
+		transformation.Translate(cubePositions[i].x, cubePositions[i].y, cubePositions[i].z);
+		float angle = 20.0f * i;
+		transformation.Rotate(glm::normalize(glm::vec3(1.0f, 0.3f, 0.5f)), angle);
+		shader.SetMatrix("uModel", transformation.GetModelMatrix());
+		renderer.Draw();
+	}
 }
--- Assets/Shaders/light-casters-frag.glsl
+++ Assets/Shaders/light-casters-frag.glsl
@@ -9,10 +9,24 @@ struct Material
 
 struct Light 
 {
-	vec3 direction;
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+};
+
+struct DirectionalLight
+{
+	Light light;
+	vec3 direction;
+};
+
+struct PointLight
+{
+	Light light;
+	vec3 position;
+	float constant;
+	float linear;
+	float quadratic;
 };
 
 in vec3 oFragmentPosition;
@@ -24,25 +38,62 @@ vec2 iTextureCoordinate = oTextureCoordinate;
 
 uniform vec3 uViewPosition;
 uniform Material uMaterial;
-uniform Light uLight;
+uniform DirectionalLight uDirectionalLight;
+uniform PointLight uPointLight;
 
 out vec4 oFragmentColor;
 
+void applyDirectionalLight();
+void applyPointLight();
+
 void main()
+{
+	//applyDirectionalLight();
+	applyPointLight();
+}
+
+void applyDirectionalLight()
 {
 	vec3 lightColor     = vec3(1.0f, 1.0f, 1.0f);
 	vec3 lightPosition  = vec3(5.0f, 5.0f, 5.0f);
 	vec3 normal         = normalize(iNormal);
-	vec3 lightDirection = normalize(-1.0f * uLight.direction);
+	vec3 lightDirection = normalize(-1.0f * uDirectionalLight.direction);
 
-	vec3 ambient = vec3(texture(uMaterial.diffuse, iTextureCoordinate)) * uLight.ambient;
+	vec3 ambient = vec3(texture(uMaterial.diffuse, iTextureCoordinate)) * uDirectionalLight.light.ambient;
 
-	vec3 diffuse =  uLight.diffuse * max(dot(normal, lightDirection), 0.0f) * vec3(texture(uMaterial.diffuse, iTextureCoordinate));
+	vec3 diffuse =  uDirectionalLight.light.diffuse * max(dot(normal, lightDirection), 0.0f) * vec3(texture(uMaterial.diffuse, iTextureCoordinate));
 
 	vec3 viewDirection     = normalize(uViewPosition - iFragmentPosition);
 	vec3 reflectDirection  = reflect(-lightDirection, normal);
-	vec3 specular          = uLight.specular * vec3(texture(uMaterial.specular, iTextureCoordinate)) * 
+	vec3 specular          = uDirectionalLight.light.specular * vec3(texture(uMaterial.specular, iTextureCoordinate)) * 
 	                         pow(max(dot(viewDirection, reflectDirection), 0.0f), uMaterial.shininess);
 
 	oFragmentColor = vec4(ambient + diffuse + specular, 1.0f);
+}
+
+void applyPointLight()
+{
+	vec3 lightColor     = vec3(1.0f, 1.0f, 1.0f);
+	vec3 lightPosition  = uPointLight.position;
+	vec3 normal         = normalize(iNormal);
+	vec3 lightDirection = normalize(lightPosition - iFragmentPosition);
+
+	vec3 ambient = vec3(texture(uMaterial.diffuse, iTextureCoordinate)) * uPointLight.light.ambient;
+
+	vec3 diffuse =  uPointLight.light.diffuse * max(dot(normal, lightDirection), 0.0f) * vec3(texture(uMaterial.diffuse, iTextureCoordinate));
+
+	vec3 viewDirection     = normalize(uViewPosition - iFragmentPosition);
+	vec3 reflectDirection  = reflect(-lightDirection, normal);
+	vec3 specular          = uPointLight.light.specular * vec3(texture(uMaterial.specular, iTextureCoordinate)) * 
+	                         pow(max(dot(viewDirection, reflectDirection), 0.0f), uMaterial.shininess);
+
+	float distance = length(uPointLight.position - iFragmentPosition);
+	float attenuation = 1.0f / ( uPointLight.constant + uPointLight.linear * distance + uPointLight.quadratic * (distance * distance) );
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
+
+	oFragmentColor = vec4(ambient + diffuse + specular, 1.0f);	
+
 }
